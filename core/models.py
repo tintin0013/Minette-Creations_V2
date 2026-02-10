08bb966e0acc4f3200f01d80039e502179ad50c8
Tintin0013 <arnaudpinatel@arnauds-MBP-001.lan>
2026-02-10 16:35:24 +0100

    Add Category model and migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
