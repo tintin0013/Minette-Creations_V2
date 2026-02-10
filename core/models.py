097b96a857ef6f831562adf45da54ca33fb9c4da
Tintin0013 <arnaudpinatel@arnauds-MBP-001.lan>
2026-02-10 16:49:18 +0100

    Add Resource model

--- core/models.py
+++ core/models.py
@@ -10,3 +10,21 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Resource(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="resources"
+    )
+
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
